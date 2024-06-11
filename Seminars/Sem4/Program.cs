f11235ae5200e9e84249c26f92a6e919dcbc2e86
oleg voronin <oleg83voronin@gmail.com>
2024-06-11 08:39:16 +0300

    We solved the first task of the fourth seminar

---
+++ Seminars/Sem4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая приниамет на вход число
+// и выдает количество цифр в числе.
+// 456 => 3
+// 78 => 2
+// 89126 => 5
+
+int DigitsCounter(int num)
+{
+    int counter = 0;
+    while (num > 0)
+    {
+        num /=10;
+        counter++;
+    }
+    return counter;
+}
+System.Console.WriteLine("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int counter = DigitsCounter(num);
+System.Console.WriteLine(counter);
