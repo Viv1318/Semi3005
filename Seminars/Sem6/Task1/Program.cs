6f0d7b12f8604de4c46b00a08e907022edc158eb
oleg voronin <oleg83voronin@gmail.com>
2024-06-19 00:15:47 +0300

    add task1

---
+++ Seminars/Sem6/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Задайте массив символов (тип char []). Создайте строку из 
+// символов этого массива.
+// Указание: конструктор строки вида string(char [])  не использовать.
+// Пример: ['a', 'b', 'c', 'd'] => "abcd"
+
+string CharToString(char[] chars)
+{
+    string result = string.Empty;//обьявили пустую строку
+    for (int i = 0; i < chars.Length; i++)
+    {
+        result += chars[i];
+    }
+    return result;
+}
+
+char[] chars = { 'a', 'b', 'c', 'd' }; // создали массив символов
+System.Console.WriteLine(CharToString(chars));
