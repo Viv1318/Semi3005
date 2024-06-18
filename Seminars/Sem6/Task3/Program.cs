b3681ce858c3c88652db2eaa3ff7036f16c20f44
oleg voronin <oleg83voronin@gmail.com>
2024-06-19 00:16:49 +0300

    add task3

---
+++ Seminars/Sem6/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿static void Main()
+{
+    System.Console.WriteLine("Введите ланиские буквы нижнего регистра:");
+    string str = Console.ReadLine();
+    char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+    int count = 0;
+    for(int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (str[i] == vowels[j])
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    System.Console.WriteLine($"Колличество гласных букв: {count}");
+}
+Main();
