bc411db3e986acc7d7b7d356378497e762fe7680
oleg voronin <oleg83voronin@gmail.com>
2024-06-19 00:16:18 +0300

    add task2

---
+++ Seminars/Sem6/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// обратная задача из строки сделать массив
+
+char[] StringToChars(string str)
+{
+    char[] result = new char[str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+        result[i] = str[i];
+    }
+    return result;
+}
+void PrintArray(char[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write("'" + array[i] + "' ");
+    }
+    System.Console.Write("");
+}
+PrintArray(StringToChars("asdfghh"));
