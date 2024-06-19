8630e04e401f0ed9fdf210b6182dfe14b6535369
oleg voronin <oleg83voronin@gmail.com>
2024-06-19 21:31:02 +0300

    add task1

---
+++ homeworks/homeworkSem6/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Задайте двумерный массив символов (тип char [,]).
+//  Создать строку из символов этого массива.
+
+char[,] array = new char[,]
+{
+    {'3', 'b', 'c'},
+    {'d', 'e', '@'},
+    {'g', '/', 'i'}
+};
+
+string result = string.Empty;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        result += array[i, j];
+    }
+}
+
+Console.WriteLine(result);
