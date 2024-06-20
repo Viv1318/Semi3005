3aa39f7672fb24a33426cf282910007b89eae259
oleg voronin <oleg83voronin@gmail.com>
2024-06-20 13:42:47 +0300

    add files

--- Seminars/Sem1/obj/Debug/net8.0/Sem1.AssemblyInfo.cs
+++ Seminars/Sem1/obj/Debug/net8.0/Sem1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3681ce858c3c88652db2eaa3ff7036f16c20f44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3251c6df620f650fcf9a2db10b5f35fe5563ea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
