c54936e22d95db66f67934679d0071203247119e
oleg voronin <oleg83voronin@gmail.com>
2024-06-20 13:40:04 +0300

    add task4

---
+++ homeworks/homeworkSem6/Task4/Program.cs
@@ -0,0 +1,48 @@
+﻿string[] GetInvertedArray(string[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        (array[array.Length - 1 - i], array[i]) = (array[i], array[array.Length - 1 - i]);
+    }
+    return array;
+}
+
+void ShowArray(string[] col)
+{
+    foreach (string item in col)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+
+}
+
+string str = "Hello my world";
+string[] arr = str.Split(" ");
+
+string[] res = GetInvertedArray(arr);
+ShowArray(res);
+
+
+// public class Task4
+// {
+//     public static void Main(string[] args)
+//     {
+//         // Входная строка со словами, разделенными пробелами
+//         string input = "Hello my world";
+//         // Вызов метода для обращения порядка слов в строке
+//         string result = ReverseWords(input);
+//         // Вывод результата
+//         Console.WriteLine(result);
+//     }
+//     // Метод для обращения порядка слов в строке
+//     public static string ReverseWords(string str)
+//     {
+//         // Разделение строки на слова
+//         string[] words = str.Split(' ');
+//         // Обращение порядка слов
+//         Array.Reverse(words);
+//         // Соединение слов обратно в строку с пробелами
+//         return string.Join(" ", words);
+//     }
+// }
