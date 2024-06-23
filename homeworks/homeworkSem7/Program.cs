b5dec4a6a9256e16444383daa4a2831dab94f6a5
oleg voronin <oleg83voronin@gmail.com>
2024-06-23 14:47:35 +0300

    add task1

---
+++ homeworks/homeworkSem7/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+System.Console.WriteLine("Input number m: ");
+int m = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Input number n: ");
+int n = Convert.ToInt32(System.Console.ReadLine());
+void OutpotN(int m, int n)
+{
+    if (m == n)
+    {
+        System.Console.Write(m);
+    }
+    if (m < n)
+    {
+        System.Console.Write(m + " ");
+       OutpotN(m + 1, n);
+    }
+    if (m > n)
+    {
+        System.Console.Write(m + " ");
+       OutpotN(m - 1, n);
+    }
+    
+}
+OutpotN(m, n);
